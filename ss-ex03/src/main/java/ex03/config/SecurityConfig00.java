36d01a71838c1286be44da0de0c1783230f89cd2
BbingDdang <ddaa1541@gmail.com>
2024-06-20 15:19:05 +0900

    FilterChain finish#01

--- ss-ex03/src/main/java/ex03/config/SecurityConfig00.java
+++ ss-ex03/src/main/java/ex03/config/SecurityConfig00.java
@@ -39,225 +39,225 @@ import java.util.List;
 
 @Configuration
 public class SecurityConfig00 {
-    @Bean
-    public FilterChainProxy springSecurityFilterChain() {
-        List<SecurityFilterChain> SecurityFilterChains = Arrays.asList(
-                new DefaultSecurityFilterChain(new AntPathRequestMatcher("/favicon.ico")),
-                new DefaultSecurityFilterChain(new AntPathRequestMatcher("/assets/**")),
-                new DefaultSecurityFilterChain(new AntPathRequestMatcher("/**"),
-                        securityContextPersistenceFilter(),
-                        logoutFilter(),
-                        usernamePasswordProcessingFilter(),
-                        anonymousAuthenticationFilter(),
-                        exceptionTranslationFilter(),
-                        filterSecurityInterceptor()
-                )
-        );
+	@Bean
+	public FilterChainProxy springSecurityFilterChain() {
+		List<SecurityFilterChain> securityFilterChains = Arrays.asList(
+			new DefaultSecurityFilterChain(new AntPathRequestMatcher("/favicon.ico")),
+			new DefaultSecurityFilterChain(new AntPathRequestMatcher("/assets/**")),
+			new DefaultSecurityFilterChain(new AntPathRequestMatcher("/**"),
+				securityContextPersistenceFilter(),
+				logoutFilter(),
+				usernamePasswordProcessingFilter(),
+				anonymousAuthenticationFilter(),
+				exceptionTranslationFilter(),
+				filterSecurityInterceptor()
+				)
+			);
 
-        return new FilterChainProxy(SecurityFilterChains);
-    }
-
-
-    /*** SecurityContextPersistenceFilter ******************/
-    @Bean
-    public SecurityContextPersistenceFilter securityContextPersistenceFilter() {
-        return new SecurityContextPersistenceFilter();
-    }
-
-    @Bean
-    public SecurityContextHolderFilter securityContextHolderFilter() {
-        return new SecurityContextHolderFilter(new DelegatingSecurityContextRepository(
-                new RequestAttributeSecurityContextRepository(),
-                new HttpSessionSecurityContextRepository()
-        ));
-    }
-
-    /*** LogoutFilter **************************************/
-    @Bean
-    public LogoutFilter logoutFilter() {
-        LogoutFilter logoutFilter = new LogoutFilter(logoutSuccessHandler(), logoutHandler());
-        logoutFilter.setFilterProcessesUrl("/user/logout");
-
-        return logoutFilter;
-    }
-
-    // LogoutSuccessHandler
-    @Bean
-    public LogoutSuccessHandler logoutSuccessHandler() {
-        SimpleUrlLogoutSuccessHandler logoutSuccessHandler = new SimpleUrlLogoutSuccessHandler();
-        logoutSuccessHandler.setDefaultTargetUrl("/");
-        logoutSuccessHandler.setAlwaysUseDefaultTargetUrl(true);
-
-        return logoutSuccessHandler;
-    }
-
-    // LogoutHandler
-    @Bean
-    public LogoutHandler logoutHandler() {
-        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
-        logoutHandler.setInvalidateHttpSession(true);
-
-        return logoutHandler;
-    }
-
-    /*** UsernamePasswordAuthenticationFilter **************/
-    @Bean
-    public UsernamePasswordAuthenticationFilter usernamePasswordProcessingFilter() {
-        UsernamePasswordAuthenticationFilter usernamePasswordProcessingFilter = new UsernamePasswordAuthenticationFilter();
-
-        usernamePasswordProcessingFilter.setAuthenticationManager(authenticationManager());
-        usernamePasswordProcessingFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
-        usernamePasswordProcessingFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
-
-        usernamePasswordProcessingFilter.setFilterProcessesUrl("/user/auth");
-        usernamePasswordProcessingFilter.setUsernameParameter("email");
-        usernamePasswordProcessingFilter.setPasswordParameter("password");
-
-        return usernamePasswordProcessingFilter;
-    }
-    
-    //
-    // AuthenticationManager
-    //
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(authenticationProvider());
-    }
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-        authenticationProvider.setPasswordEncoder(passwordEncoder());
-        authenticationProvider.setUserDetailsService(userDetailsService());
-
-        return authenticationProvider;
-    }
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(16);
-    }
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return new UserDetailsService(){
-
-            @Override
-            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                return null;
-            }
-        };
-    }
-
-    //
-    // AuthenticationSuccessHandler
-    //
-    @Bean
-    public AuthenticationSuccessHandler authenticationSuccessHandler() {
-        SavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-        authenticationSuccessHandler.setDefaultTargetUrl("/");
-
-        return authenticationSuccessHandler;
-    }
-    
-    //
-    // AuthenticationFailureHandler
-    //
-    @Bean
-    public AuthenticationFailureHandler authenticationFailureHandler() {
-        SimpleUrlAuthenticationFailureHandler authenticationFailureHandler = new SimpleUrlAuthenticationFailureHandler();
-        authenticationFailureHandler.setDefaultFailureUrl("/user/login");
-
-        return authenticationFailureHandler;
-    }
+		return new FilterChainProxy(securityFilterChains);
+	}
 
 
-    /*** AnonymousAuthenticationFilter **********************/
-    @Bean
-    public AnonymousAuthenticationFilter anonymousAuthenticationFilter() {
-        return new AnonymousAuthenticationFilter("BF93JFJ091N00Q7HF");
-    }
+	/*** SecurityContextPersistenceFilter ******************/
+	@Bean
+	public SecurityContextPersistenceFilter securityContextPersistenceFilter() {
+		return new SecurityContextPersistenceFilter();
+	}
+
+	@Bean
+	public SecurityContextHolderFilter securityContextHolderFilter() {
+		return new SecurityContextHolderFilter(new DelegatingSecurityContextRepository(
+				new RequestAttributeSecurityContextRepository(),
+				new HttpSessionSecurityContextRepository()
+				));
+	}
+
+	/*** LogoutFilter **************************************/
+	@Bean
+	public LogoutFilter logoutFilter() {
+		LogoutFilter logoutFilter = new LogoutFilter(logoutSuccessHandler(), logoutHandler());
+		logoutFilter.setFilterProcessesUrl("/user/logout"); // 어떤경로로 오면 logout할건지 설정
+
+		return logoutFilter;
+	}
+
+	// LogoutSuccessHandler
+	@Bean
+	public LogoutSuccessHandler logoutSuccessHandler() {
+		SimpleUrlLogoutSuccessHandler logoutSuccessHandler = new SimpleUrlLogoutSuccessHandler();
+		logoutSuccessHandler.setDefaultTargetUrl("/");
+		logoutSuccessHandler.setAlwaysUseDefaultTargetUrl(true);
+
+		return logoutSuccessHandler;
+	}
+
+	// LogoutHandler
+	@Bean
+	public LogoutHandler logoutHandler() {
+		SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+		logoutHandler.setInvalidateHttpSession(true);
+
+		return logoutHandler;
+	}
+
+	/*** UsernamePasswordAuthenticationFilter **************/
+	@Bean
+	public UsernamePasswordAuthenticationFilter usernamePasswordProcessingFilter() {
+		UsernamePasswordAuthenticationFilter usernamePasswordProcessingFilter = new UsernamePasswordAuthenticationFilter();
+
+		usernamePasswordProcessingFilter.setAuthenticationManager(authenticationManager());
+		usernamePasswordProcessingFilter.setAuthenticationSuccessHandler(authenticationSuccessHandler());
+		usernamePasswordProcessingFilter.setAuthenticationFailureHandler(authenticationFailureHandler());
+
+		usernamePasswordProcessingFilter.setFilterProcessesUrl("/user/auth");
+		usernamePasswordProcessingFilter.setUsernameParameter("email");
+		usernamePasswordProcessingFilter.setPasswordParameter("password");
+
+		return usernamePasswordProcessingFilter;
+	}
+
+	//
+	// AuthenticationManager
+	//
+	@Bean
+	public AuthenticationManager authenticationManager() {
+		return new ProviderManager(authenticationProvider());
+	}
+	@Bean
+	public AuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		authenticationProvider.setUserDetailsService(userDetailsService());
+
+		return authenticationProvider;
+	}
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(16);
+	}
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new UserDetailsService(){
+
+			@Override
+			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+				return null;
+			}
+		};
+	}
+
+	//
+	// AuthenticationSuccessHandler
+	//
+	@Bean
+	public AuthenticationSuccessHandler authenticationSuccessHandler() {
+		SavedRequestAwareAuthenticationSuccessHandler authenticationSuccessHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+		authenticationSuccessHandler.setDefaultTargetUrl("/");
+
+		return authenticationSuccessHandler;
+	}
+
+	//
+	// AuthenticationFailureHandler
+	//
+	@Bean
+	public AuthenticationFailureHandler authenticationFailureHandler() {
+		SimpleUrlAuthenticationFailureHandler authenticationFailureHandler = new SimpleUrlAuthenticationFailureHandler();
+		authenticationFailureHandler.setDefaultFailureUrl("/user/login");
+
+		return authenticationFailureHandler;
+	}
 
 
-    /*** ExceptionTranslationFilter *************************/
-    @Bean
-    public ExceptionTranslationFilter exceptionTranslationFilter() {
-        ExceptionTranslationFilter exceptionTranslationFilter = new ExceptionTranslationFilter(authenticationEntryPoint());
-        exceptionTranslationFilter.setAccessDeniedHandler(accessDeniedHandler());
-
-        return exceptionTranslationFilter;
-    }
-
-    //
-    // AuthenticationEntryPoint
-    //
-    @Bean
-    public AuthenticationEntryPoint authenticationEntryPoint() {
-        return new LoginUrlAuthenticationEntryPoint("/user/login");
-    }
-
-    //
-    // AccessDeniedHandler
-    //
-    @Bean
-    public AccessDeniedHandler accessDeniedHandler() {
-        AccessDeniedHandlerImpl accessDeniedHandler = new AccessDeniedHandlerImpl();
-        accessDeniedHandler.setErrorPage("/WEB-INF/views/error/403.jsp");
-
-        return accessDeniedHandler;
-    }
+	/*** AnonymousAuthenticationFilter **********************/
+	@Bean
+	public AnonymousAuthenticationFilter anonymousAuthenticationFilter() {
+		return new AnonymousAuthenticationFilter("BF93JFJ091N00Q7HF");
+	}
 
 
-    /*** FilterSecurityInterceptor *************************/
-    @Bean
-    public FilterSecurityInterceptor filterSecurityInterceptor() {
-        FilterSecurityInterceptor filterSecurityInterceptor = new FilterSecurityInterceptor();
-        filterSecurityInterceptor.setAuthenticationManager(authenticationManager());
-        filterSecurityInterceptor.setSecurityMetadataSource(filterInvocationSecurityMetadataSource());
-        filterSecurityInterceptor.setAccessDecisionManager(accessDecisionManager());
+	/*** ExceptionTranslationFilter *************************/
+	@Bean
+	public ExceptionTranslationFilter exceptionTranslationFilter() {
+		ExceptionTranslationFilter exceptionTranslationFilter = new ExceptionTranslationFilter(authenticationEntryPoint());
+		exceptionTranslationFilter.setAccessDeniedHandler(accessDeniedHandler());
 
-        return filterSecurityInterceptor;
-    }
+		return exceptionTranslationFilter;
+	}
 
-    //
-    // FilterSecurityInterceptor
-    //
-    @Bean
-    public FilterInvocationSecurityMetadataSource filterInvocationSecurityMetadataSource() {
-        return new DefaultFilterInvocationSecurityMetadataSource(new LinkedHashMap<>(){{
-            put(new RegexRequestMatcher("^/admin/?.*$", null), Arrays.asList(() -> "ROLE_ADMIN"));
-            put(new RegexRequestMatcher("^/board/?(write|reply|delete)$", null), Arrays.asList(() -> "ROLE_USER"));
-            put(new RegexRequestMatcher("^/user/update$", null), Arrays.asList(() -> "ROLE_USER"));
-        }});
-    }
+	//
+	// AuthenticationEntryPoint
+	//
+	@Bean
+	public AuthenticationEntryPoint authenticationEntryPoint() {
+		return new LoginUrlAuthenticationEntryPoint("/user/login");
+	}
 
-    //
-    // AccessDecisionManager
-    //
-    @Bean
-    public AccessDecisionManager accessDecisionManager() {
-        return new AffirmativeBased(Arrays.asList(
-                authenticatedVoter(),
-                roleVoter(),
-                webExpressionVoter()
+	//
+	// AccessDeniedHandler
+	//
+	@Bean
+	public AccessDeniedHandler accessDeniedHandler() {
+		AccessDeniedHandlerImpl accessDeniedHandler = new AccessDeniedHandlerImpl();
+		accessDeniedHandler.setErrorPage("/WEB-INF/views/error/403.jsp");
 
-        ));
-    }
+		return accessDeniedHandler;
+	}
 
-    @Bean
-    public AuthenticatedVoter authenticatedVoter() {
-        return new AuthenticatedVoter();
-    }
-    @Bean
-    public RoleVoter roleVoter() {
-        return new RoleVoter();
-    }
-    @Bean
-    public WebExpressionVoter webExpressionVoter() {
-        WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
-        webExpressionVoter.setExpressionHandler(securityExpressionHandler());
 
-        return webExpressionVoter;
-    }
-    @Bean
-    public SecurityExpressionHandler securityExpressionHandler() {
-        return new DefaultWebSecurityExpressionHandler();
-    }
+	/*** FilterSecurityInterceptor *************************/
+	@Bean
+	public FilterSecurityInterceptor filterSecurityInterceptor() {
+		FilterSecurityInterceptor filterSecurityInterceptor = new FilterSecurityInterceptor();
+		filterSecurityInterceptor.setAuthenticationManager(authenticationManager());
+		filterSecurityInterceptor.setSecurityMetadataSource(filterInvocationSecurityMetadataSource());
+		filterSecurityInterceptor.setAccessDecisionManager(accessDecisionManager());
+
+		return filterSecurityInterceptor;
+	}
+
+	//
+	// FilterSecurityInterceptor
+	//
+	@Bean
+	public FilterInvocationSecurityMetadataSource filterInvocationSecurityMetadataSource() {
+		return new DefaultFilterInvocationSecurityMetadataSource(new LinkedHashMap<>(){{
+			put(new RegexRequestMatcher("^/admin/?.*$", null), Arrays.asList(() -> "ROLE_ADMIN"));
+			put(new RegexRequestMatcher("^/board/?(write|reply|delete)$", null), Arrays.asList(() -> "ROLE_USER"));
+			put(new RegexRequestMatcher("^/user/update$", null), Arrays.asList(() -> "ROLE_USER"));
+		}});
+	}
+
+	//
+	// AccessDecisionManager
+	//
+	@Bean
+	public AccessDecisionManager accessDecisionManager() {
+		return new AffirmativeBased(Arrays.asList(
+				authenticatedVoter(),
+				roleVoter(),
+				webExpressionVoter()
+
+				));
+	}
+
+	@Bean
+	public AuthenticatedVoter authenticatedVoter() {
+		return new AuthenticatedVoter();
+	}
+	@Bean
+	public RoleVoter roleVoter() {
+		return new RoleVoter();
+	}
+	@Bean
+	public WebExpressionVoter webExpressionVoter() {
+		WebExpressionVoter webExpressionVoter = new WebExpressionVoter();
+		webExpressionVoter.setExpressionHandler(securityExpressionHandler());
+
+		return webExpressionVoter;
+	}
+	@Bean
+	public SecurityExpressionHandler securityExpressionHandler() {
+		return new DefaultWebSecurityExpressionHandler();
+	}
 }
